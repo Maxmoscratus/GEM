b031607dc85bae1c81f6384c85ca6a906707336d
Maxmoscratus <jorge76168@gmail.com>
2024-06-08 03:57:53 +0000

    finish `/api/v1/status`endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,12 +6,11 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnecttionsResult = await database.query(
+  const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
-
-  const databaseMaxConnecttionsValue =
-    databaseMaxConnecttionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
@@ -26,7 +25,7 @@ async function status(request, response) {
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnecttionsValue),
+        max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
       },
     },
