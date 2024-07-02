957e713c5a6c606440ed03fffcd5c53824696c59
Maxmoscratus <jorge76168@gmail.com>
2024-07-02 19:36:55 +0000

    add `modified pages/index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <h1>
-      Bom dia Maj. Barreto! Preciso de sua ajuda para concretizar este projeto!
-      Página Web estruturada/Com segurança de ponta/autorização DGTIC/Hospedagem
-      no DataCenter Rio.
+      Boa tarde Coronel Lenise! Preciso de sua ajuda para concretizar este
+      projeto! Página Web estruturada/Com segurança de ponta/autorização
+      DGTIC/Hospedagem no DataCenter Rio.
     </h1>
   );
 }
